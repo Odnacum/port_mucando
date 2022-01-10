15280fc9357dbf15a5022a1a68e71ab70a3f0042
Mucando <mucando@hotmail.com>
2022-01-10 10:38:21 +0200

    Modificacoes finais

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,3 @@
-import uniqid from "uniqid";
 import { contact } from "../../portfolio";
 import "./Contact.css";
 
@@ -11,33 +10,29 @@ const Contact = () => {
         <h2 className="section__title">Contacto e Endereço</h2>
       </div>
 
-      <div className="row mt-2 me-2">
-        <span type="button" className="btn btn--outline  col-3 m-1">
+      <div className="row mt-2 me-2 py-lg-5">
+        <span type="button" className="btn btn--outline  col-sm-4">
           <span className="text-capitalize text-black">Conctacto:</span> {contact.map((contacto) => (contacto.phone))}
         </span>
-        <span type="button" className="btn btn--outline col-5 m-1">
+        <span type="button" className="btn btn--outline col-sm-4">
          <span className="text-capitalize text-black">Morada:</span> {contact.map((contacto) => (contacto.morada))}
         </span>
-        <span type="button" className="btn btn--outline col-3 m-1">
+        <span type="button" className="btn btn--outline col-sm-4">
            <span className="text-capitalize text-black">Email:</span> {contact.map((contacto) => (contacto.email))}
         </span>
       </div>
-      <div className="dif row text-center mt-5">
-        <div className="col-md-2 ">
-          <a href={`tel:${contact.map((contacto) => (contacto.phone))}`} className="alert alert-primary ms-4 border ">
-        <span type="button">
+      <div className="row mt-2 me-2 py-lg-5 text-center">
+        <span type="button" className="btn btn-sm btn-link col-sm-4 text-center mb-3">
+         <a href={`tel:${contact.map((contacto) => (contacto.phone))}`} className="alert alert-primary ms-4 border ">
           Ligue-me
-        </span>
           </a>
-        </div><br/>
-        <div className="col-md-3">
-          <a href={`mailto:${contact.map((contacto) => (contacto.email))}`}
-             className="alert alert-primary me-1 border ">
-        <span type="button">
+        </span>
+        <span type="button" className="btn btn-sm btn-link  col-sm-4 text-sm-start">
+           <a href={`mailto:${contact.map((contacto) => (contacto.email))}`}
+              className="alert alert-primary me-1 border ">
           Envie-me um email
-        </span>
           </a>
-        </div>
+        </span>
       </div>
 
 
